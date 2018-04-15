af4b7011d55f861b24b2b77d66a659e17f358475
Redi Enterprises <Redi Enterprises@RediEnterprises>
2018-04-15 17:27:18 -0400

    Choice of animal scratch update

---
+++ Code/Scratches/AH Choice of animal/core/src/gdx/menu/GamMenu.java
@@ -0,0 +1,70 @@
+package gdx.menu;
+import com.badlogic.gdx.Game;
+import gdx.menu.Screens.ScrMenu;
+import gdx.menu.Screens.ScrTail;
+import gdx.menu.Screens.ScrFood;
+import gdx.menu.Screens.ScrAnimation;
+import gdx.menu.Screens.ScrAniHit;
+import gdx.menu.Screens.ScrGame;
+import gdx.menu.Screens.ScrGameOver;
+import gdx.menu.Screens.ScrAnimalChoice;
+
+
+
+public class GamMenu extends Game {
+    ScrMenu scrMenu;
+    ScrTail scrTail;
+    ScrFood scrFood;
+    ScrAnimation scrAnimation;
+    ScrAniHit scrAniHit;
+    ScrGame scrGame;
+    ScrGameOver scrGameOver;
+    ScrAnimalChoice scrAnimalChoice;
+    int nScreen; // 0 for menu, 1 for play, 2 for Sign, 3 for Animation, 4 for AniHit, 5 for Game, 6 for GameOver, 7 for AnimalChoice
+    
+    public void updateState(int _nScreen) {
+        nScreen = _nScreen;
+        if ( nScreen == 0) {
+            setScreen(scrMenu);
+        } else if (nScreen == 1) {
+            setScreen(scrTail);
+        } else if (nScreen ==2) {
+            setScreen(scrFood);
+        } else if (nScreen == 3){
+            setScreen(scrAnimation);
+        } else if (nScreen == 4){
+            setScreen(scrAniHit);
+        } else if (nScreen == 5){
+            setScreen(scrGame);
+        } else if (nScreen == 6){
+            setScreen(scrGameOver);
+        } else if (nScreen == 7) {
+            setScreen(scrAnimalChoice);
+        }
+    }
+
+    @Override
+    public void create() {
+        nScreen = 0;
+        // notice that "this" is passed to each screen. Each screen now has access to methods within the "game" master program
+        scrMenu = new ScrMenu(this);
+        scrTail = new ScrTail(this);
+        scrFood = new ScrFood(this);
+        scrAnimation = new ScrAnimation(this);
+        scrAniHit = new ScrAniHit(this);
+        scrGame = new ScrGame(this);
+        scrGameOver = new ScrGameOver(this);
+        scrAnimalChoice = new ScrAnimalChoice(this);
+        updateState(0);
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
---
+++ Code/Scratches/AH Choice of animal/core/src/gdx/menu/Screens/ScrMenu.java
@@ -0,0 +1,160 @@
+package gdx.menu.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import gdx.menu.GamMenu;
+import gdx.menu.images.Button;
+
+public class ScrMenu implements Screen, InputProcessor {
+
+    Button btnPlay, btnAni, btnSign, btnQuit, btnAH, btnGame;
+    GamMenu gamMenu;
+    Texture txNamM, txBackground;
+    OrthographicCamera oc;
+    SpriteBatch batch;
+    Sprite sprNamM,sprBackground;
+
+    public ScrMenu(GamMenu _gamMenu) {  //Referencing the main class.
+        gamMenu = _gamMenu;
+    }
+
+    @Override
+    public void show() {
+        oc = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        oc.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        oc.update();
+        batch = new SpriteBatch();
+        btnPlay = new Button(100, 50, 0, Gdx.graphics.getHeight() - 50, "Tail.png");
+        btnAni = new Button(100, 50, Gdx.graphics.getWidth() - 100, Gdx.graphics.getHeight() - 50, "Animation.jpg");
+        btnSign = new Button(100, 50, Gdx.graphics.getWidth() / 2 - 50, Gdx.graphics.getHeight() - 50, "Food.png");
+        btnQuit = new Button(100, 50, Gdx.graphics.getWidth() - 100, 0, "Quit.jpg");
+        btnAH = new Button(100, 50, Gdx.graphics.getWidth()/2 - 50, 0, "AniHit.png");
+        btnGame = new Button(100, 50, 0, 0, "Game.png");
+        txBackground = new Texture ("EarthBG.jpg");
+        sprBackground = new Sprite (txBackground);
+        sprBackground.setFlip(false, true);
+        sprBackground.setScale(4.05f,2.75f);
+        txNamM = new Texture("M.jpg");
+        sprNamM = new Sprite(txNamM);
+        sprNamM.setFlip(false, true);
+        sprNamM.setSize(60, 80);
+        sprNamM.setPosition(Gdx.graphics.getWidth() / 2 - 30, Gdx.graphics.getHeight() / 2 - 40);
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,0); //black background
+        
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.setProjectionMatrix(oc.combined);
+        sprBackground.draw(batch);
+        sprNamM.draw(batch);
+        btnPlay.draw(batch);
+        btnAni.draw(batch);
+        btnSign.draw(batch);
+        btnQuit.draw(batch);
+        btnAH.draw(batch);
+        btnGame.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        txNamM.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if (button == Input.Buttons.LEFT) {
+            if (isHit(screenX, screenY, btnPlay)) {
+                System.out.println("Hit Play");
+                gamMenu.updateState(1);
+            } else if (isHit(screenX, screenY, btnAni)) {
+                System.out.println("Hit Animation");
+                gamMenu.updateState(3);
+            } else if(isHit(screenX, screenY, btnSign)){
+                System.out.println("Hit Food");
+                gamMenu.updateState(2);
+            } else if(isHit(screenX, screenY, btnQuit)){
+                System.out.println("Quit");
+                System.exit(0);
+            } else if(isHit(screenX, screenY, btnAH)){
+                System.out.println("Hit AniHit");
+                gamMenu.updateState(4);
+            } else if(isHit(screenX, screenY, btnGame)){
+                System.out.println("Hit Game");
+                gamMenu.updateState(7);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
+    public boolean isHit(int nX, int nY, Sprite sprBtn) {
+        if (nX > sprBtn.getX() && nX < sprBtn.getX() + sprBtn.getWidth() && nY > sprBtn.getY() && nY < sprBtn.getY() + sprBtn.getHeight()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
