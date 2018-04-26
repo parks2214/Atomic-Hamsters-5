01d95b835860b9f89333087f2fb2ba6847be6821
Scott Park <parks2214@WRDSB.CA>
2018-04-26 10:55:46 -0400

    Adding poison to stuff

---
+++ Code/Scratches/AH Randomly generated food/core/src/gdx/menu/images/Poison.java
@@ -0,0 +1,20 @@
+package gdx.menu.images;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Poison extends Sprite {
+    float fW, fH;
+
+    public Poison(float fX, float fY, Texture texture) {
+        super(texture);
+        fW = 50;//this is what it is set too in scrFood, want natural size later
+        fH = 50;
+        // fW = texture.getWidth(); //code ready to just directly take size here
+        //fH = texture.getHeight();
+        setPosition(fX, fY); //set
+        setFlip(false, true);
+        setSize(fW, fH); //set this in Pellet maker
+    }
+
+}
