3145e0a99631b0239a8df78bacd5d644489cb945
Scott Park <parks2214@WRDSB.CA>
2018-05-11 10:05:13 -0400

    We now have a working version that should have a build.gradle

---
+++ Code/Releases/AH Version 4.1/desktop/src/gdx/menu/desktop/DesktopLauncher.java
@@ -0,0 +1,24 @@
+package gdx.menu.desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Scaling;
+import gdx.menu.GamMenu;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height= 480;
+		config.width = 640;
+		config.vSyncEnabled = true;
+
+		new LwjglApplication(new GamMenu(), config);
+	}
+
+}
